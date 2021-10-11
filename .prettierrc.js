df46ed1dea1f7dcf7d184ea0729e1553b1ee345b
ling <971039649@qq.com>
2021-10-11 17:52:59 +0800

    prettier格式化配置

--- .prettierrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  printWidth: 120,
-  htmlWhitespaceSensitivity: 'ignore',
-  singleQuote: true,
-  trailingComma: 'none',
-  bracketSpacing: true,
-  semi: false,
-  jsxBracketSameLine: false
-}
